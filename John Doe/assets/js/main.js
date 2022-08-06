55ba2599b2c5c6dbecb0cfb68e3543c4967c68ca
Əşrəf <101041982+ashraf032@users.noreply.github.com>
2022-08-06 17:48:57 +0400

    salam

---
+++ John Doe/assets/js/main.js
@@ -0,0 +1,33 @@
+document.body.style.background= "black";
+document.body.style.color="white";
+document.body.style.fontFamily= "Montserrat", "sans-serif";
+
+
+
+document.getElementById("j-doe").style.display = "flex";
+document.getElementById("j-doe").style.justifyContent = "center";
+
+document.getElementById("j-do").style.display = "flex";
+document.getElementById("j-do").style.justifyContent = "center";
+
+// document.getElementById("j-doe").style.display = "flex";
+// document.getElementById("j-doe").style.justifyContent = "center";
+
+document.getElementById("john").style.display= "";
+document.getElementById("john").style.justifyContent="center";
+
+
+
+const listItem = document.querySelector(`ul`);
+listItem.style.listStyle= "none";
+listItem.style.position = "fixed";
+listItem.style.justifyContent = "space-beetween";
+
+
+// listItem.style.border= "solid";
+
+
+
+
+
+
